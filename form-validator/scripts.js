3a3514e06940fcadb3535558e913ed14d01b1c37
soonbee <maru204@gmail.com>
2023-02-25 15:06:36 +0900

    Add validation for required value

--- form-validator/scripts.js
+++ form-validator/scripts.js
@@ -1,1 +1,58 @@
-/* empty */
+let form = document.getElementsByTagName('form')[0];
+let username = document.getElementsByName('username')[0];
+let email = document.getElementsByName('email')[0];
+let password = document.getElementsByName('password')[0];
+let confirmPassword = document.getElementsByName('confirm')[0];
+
+/**
+ * 
+ * @param {string} value 
+ * @returns {string}
+ */
+function capitalize(value) {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+/**
+ * 
+ * @param {HTMLElement} target 
+ * @returns {boolean}
+ */
+function checkRequired(target) {
+  if (!target.value) {
+    let parent = target.parentElement;
+    parent.classList.add('error');
+
+    let name = target.getAttribute('name');
+    let message = parent.getElementsByClassName('error-message')[0];
+    message.textContent = `${capitalize(name)} is required.`;
+    return false;
+  }
+  return true;
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const valid = checkRequired(username)
+  && checkRequired(email)
+  && checkRequired(password)
+  && checkRequired(confirmPassword);
+
+  if (valid) {
+    // TODO: register user
+  }
+});
+
+function resetError() {
+  let parent = this.parentElement;
+  parent.classList.remove('error');
+  let message = parent.getElementsByClassName('error-message')[0];
+  message.textContent = '';
+}
+
+username.addEventListener('input', resetError);
+email.addEventListener('input', resetError);
+password.addEventListener('input', resetError);
+confirmPassword.addEventListener('input', resetError);
+
