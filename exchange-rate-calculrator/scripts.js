3a340e33897dde71a0fc2955d807ecba7af5a1f4
soonbee <maru204@gmail.com>
2023-03-05 15:48:19 +0900

    add exchange rate calculator code

--- exchange-rate-calculrator/scripts.js
+++ exchange-rate-calculrator/scripts.js
@@ -1,1 +1,40 @@
-/* empty */
+let baseSelect = document.querySelector("#currency-base > select");
+let baseInput = document.querySelector("#currency-base > input");
+let targetSelect = document.querySelector("#currency-target > select");
+let targetInput = document.querySelector("#currency-target > input");
+let rateDisplay = document.querySelector("#rate > span");
+let swapButton = document.querySelector("#rate > button");
+let rate = 0;
+
+async function updateRate() {
+  let base = baseSelect.value;
+  let target = targetSelect.value;
+  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${base}/${target}.min.json`
+  let response = await fetch(url);
+  let json = await response.json();
+  rate = json[target];
+  rateDisplay.textContent = `1 ${base.toUpperCase()} = ${rate} ${target.toUpperCase()}`;
+  targetInput.value = (Number(baseInput.value) * Number(rate)).toFixed(2);
+}
+
+window.addEventListener("load", function() {
+  baseInput.value = "1"
+  baseSelect.value = "usd";
+  targetSelect.value = "eur";
+  updateRate();
+})
+
+baseSelect.addEventListener("change", updateRate);
+targetSelect.addEventListener("change", updateRate);
+
+baseInput.addEventListener("input", function() {
+  targetInput.value = (Number(baseInput.value) * Number(rate)).toFixed(2);
+});
+
+swapButton.addEventListener("click", function() {
+  let base = baseSelect.value;
+  let target = targetSelect.value;
+  baseSelect.value = target;
+  targetSelect.value = base;
+  updateRate();
+});
