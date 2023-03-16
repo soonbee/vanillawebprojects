aa074215965c7e074a9176195d8c9d6a84c960c0
soonbee <maru204@gmail.com>
2023-03-16 13:00:38 +0900

    Add sidebar

--- modal-menu-slider/scripts.js
+++ modal-menu-slider/scripts.js
@@ -1,1 +1,18 @@
-/* empty */
+let menu = document.getElementById("menu");
+let sidebar = document.querySelector("aside");
+let main = document.querySelector("main");
+let isOpen = false;
+
+menu.addEventListener("click", function() {
+  if (isOpen) {
+    sidebar.removeAttribute("open");
+    sidebar.style.transform = "translate(0px, 0)";
+    main.style.marginLeft = "0px";
+    isOpen = !isOpen;
+  } else {
+    sidebar.setAttribute("open", "");
+    sidebar.style.transform = "translate(200px, 0)";
+    main.style.marginLeft = "200px";
+    isOpen = !isOpen;
+  }
+});
