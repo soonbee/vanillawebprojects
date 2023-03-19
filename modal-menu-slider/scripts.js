1a9a017c8104e5c1c07a3c8a070116971fefdc6f
soonbee <maru204@gmail.com>
2023-03-19 15:21:49 +0900

    Add sign up modal

--- modal-menu-slider/scripts.js
+++ modal-menu-slider/scripts.js
@@ -1,18 +1,35 @@
 let menu = document.getElementById("menu");
 let sidebar = document.querySelector("aside");
 let main = document.querySelector("main");
-let isOpen = false;
+let modal = document.querySelector(".modal");
+let signUpButton = document.querySelector("#sign-up");
+let closeButton = document.querySelector("#modal-close");
+let submitButton = document.querySelector("#modal-submit");
+let isSidebarOpen = false;
+let isModalOpen = false;
 
 menu.addEventListener("click", function() {
-  if (isOpen) {
+  if (isSidebarOpen) {
     sidebar.removeAttribute("open");
     sidebar.style.transform = "translate(0px, 0)";
     main.style.marginLeft = "0px";
-    isOpen = !isOpen;
+    isSidebarOpen = !isSidebarOpen;
   } else {
     sidebar.setAttribute("open", "");
     sidebar.style.transform = "translate(200px, 0)";
     main.style.marginLeft = "200px";
-    isOpen = !isOpen;
+    isSidebarOpen = !isSidebarOpen;
   }
 });
+
+function closeModal() {
+  modal.removeAttribute("show");
+}
+function openModal() {
+  modal.setAttribute("show", "");
+}
+
+signUpButton.onclick = openModal;
+closeButton.onclick = closeModal;
+submitButton.onclick = closeModal;
+
