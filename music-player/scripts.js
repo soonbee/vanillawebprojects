f96fed590ea060472889d90014bb18730dd9c353
soonbee <maru204@gmail.com>
2023-03-29 18:13:21 +0900

    Add audio control feature

--- music-player/scripts.js
+++ music-player/scripts.js
@@ -1,14 +1,50 @@
-let isPlay = false;
 let playButton = document.querySelector("#play-button");
-let musicPlayer = document.getElementById("music-player");
+let musicPlayer = document.querySelector("#music-player");
+let musicSource = document.querySelector("#music-source");
+let musicTitle = document.querySelector("#music-title");
+let musicCover = document.querySelector("#music-cover");
+
+let currentIndex = 0;
+let sourceList = [
+    {title: "hey", src: "music/music-player_music_hey.mp3", img: "images/hey.jpeg"},
+    {title: "summer", src: "music/music-player_music_summer.mp3", img: "images/summer.jpeg"},
+    {title: "ukulele", src: "music/music-player_music_ukulele.mp3", img: "images/ukulele.jpeg"},
+];
 
 playButton.onclick = function() {
-    if (isPlay) {
-        playButton.innerHTML = `<i class="fas fa-play fa-2x" style="color: #cdc2d0;"></i>`;
-        musicPlayer.removeAttribute("play");
+    if (musicSource.paused) {
+        musicSource.play();
     } else {
-        playButton.innerHTML = `<i class="fas fa-pause fa-2x" style="color: #cdc2d0;"></i>`;
-        musicPlayer.setAttribute("play", "");
+        musicSource.pause();
     }
-    isPlay = !isPlay
-}
+};
+
+window.addEventListener("load", function() {
+    musicTitle.textContent = sourceList[currentIndex].title;
+    musicCover.src = sourceList[currentIndex].img;
+    musicSource.src = sourceList[currentIndex].src;
+});
+
+musicSource.onplay = function() {
+    playButton.innerHTML = `<i class="fas fa-pause fa-2x" style="color: #cdc2d0;"></i>`;
+    musicPlayer.setAttribute("play", "");
+};
+musicSource.onpause = function() {
+    playButton.innerHTML = `<i class="fas fa-play fa-2x" style="color: #cdc2d0;"></i>`;
+    musicPlayer.removeAttribute("play");
+};
+
+document.querySelector("#backward-button").onclick = function() {
+    currentIndex = (currentIndex + 3 - 1) % 3;
+    musicTitle.textContent = sourceList[currentIndex].title;
+    musicCover.src = sourceList[currentIndex].img;
+    musicSource.src = sourceList[currentIndex].src;
+    musicSource.play();
+};
+document.querySelector("#forward-button").onclick = function() {
+    currentIndex = (currentIndex + 3 + 1) % 3;
+    musicTitle.textContent = sourceList[currentIndex].title;
+    musicCover.src = sourceList[currentIndex].img;
+    musicSource.src = sourceList[currentIndex].src;
+    musicSource.play();
+};
