920107b96842e37e696334c4ff6fa75b8bd88684
soonbee <maru204@gmail.com>
2023-03-29 17:20:26 +0900

    Add spin animation

--- music-player/scripts.js
+++ music-player/scripts.js
@@ -1,11 +1,14 @@
 let isPlay = false;
 let playButton = document.querySelector("#play-button");
+let musicPlayer = document.getElementById("music-player");
 
 playButton.onclick = function() {
     if (isPlay) {
         playButton.innerHTML = `<i class="fas fa-play fa-2x" style="color: #cdc2d0;"></i>`;
+        musicPlayer.removeAttribute("play");
     } else {
         playButton.innerHTML = `<i class="fas fa-pause fa-2x" style="color: #cdc2d0;"></i>`;
+        musicPlayer.setAttribute("play", "");
     }
     isPlay = !isPlay
 }
