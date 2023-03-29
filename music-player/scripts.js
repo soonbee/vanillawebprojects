20e6e8e0bc4e99861f0fcc7f9f126f4512063907
soonbee <maru204@gmail.com>
2023-03-29 17:20:16 +0900

    Add music player ui

--- music-player/scripts.js
+++ music-player/scripts.js
@@ -1,1 +1,11 @@
-/* empty */
+let isPlay = false;
+let playButton = document.querySelector("#play-button");
+
+playButton.onclick = function() {
+    if (isPlay) {
+        playButton.innerHTML = `<i class="fas fa-play fa-2x" style="color: #cdc2d0;"></i>`;
+    } else {
+        playButton.innerHTML = `<i class="fas fa-pause fa-2x" style="color: #cdc2d0;"></i>`;
+    }
+    isPlay = !isPlay
+}
