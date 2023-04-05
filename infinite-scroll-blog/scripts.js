5aba7cdb5e759662f400557d9c52472a650d34ea
soonbee <maru204@gmail.com>
2023-04-05 12:56:53 +0900

    Update to fetch post

--- infinite-scroll-blog/scripts.js
+++ infinite-scroll-blog/scripts.js
@@ -1,1 +1,35 @@
-/* empty */
+let mainEl = document.querySelector("main");
+let limit = 5;
+let page = 1;
+
+async function fetchPostList() {
+    let url = `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`;
+    let response = await fetch(url);
+    let json = await response.json();
+    return json;
+};
+
+function generatePostElement(id, title, body) {
+    let postEl = document.createElement("div");
+    let postIndexEl = document.createElement("div");
+    let postTitleEl = document.createElement("h2");
+    let postBodyEl = document.createElement("p");
+    let a = postEl.classList.add("post");
+    postIndexEl.classList.add("post-index");
+    postIndexEl.textContent = id;
+    postTitleEl.textContent = title;
+    postBodyEl.textContent = body;
+    postEl.appendChild(postIndexEl);
+    postEl.appendChild(postTitleEl);
+    postEl.appendChild(postBodyEl);
+    return postEl;
+}
+
+async function loadPost() {
+    let posts = await fetchPostList();
+    posts.forEach(({id, title, body}) => {
+        let postEl = generatePostElement(String(id), title, body);
+        mainEl.appendChild(postEl);
+    })
+    page++;
+}
