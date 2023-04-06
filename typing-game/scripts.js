3fecd6f51eb6144df34a45306e3652525ccf5041
soonbee <maru204@gmail.com>
2023-04-06 14:26:35 +0900

    Add random word generator

--- typing-game/scripts.js
+++ typing-game/scripts.js
@@ -1,1 +1,36 @@
-/* empty */
+function* RandomWordGenerator() {
+    const words = [
+        'sigh',
+        'tense',
+        'airplane',
+        'ball',
+        'pies',
+        'juice',
+        'warlike',
+        'bad',
+        'north',
+        'dependent',
+        'steer',
+        'silver',
+        'highfalutin',
+        'superficial',
+        'quince',
+        'eight',
+        'feeble',
+        'admit',
+        'drag',
+        'loving'
+    ].sort(() => Math.random() - 0.5);
+    let cur = 0;
+    while (cur < words.length) {
+        yield words[cur++];
+    }
+}
+const randomWordGenerator = RandomWordGenerator();
+
+function startGame() {
+    const nextWord = randomWordGenerator.next().value;   
+    document.querySelector("#game-target").textContent = nextWord;
+}
+
+startGame();
