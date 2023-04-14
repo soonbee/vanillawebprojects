60ebb2bd6e19eb5e713a9d177f31b05251d5437e
soonbee <maru204@gmail.com>
2023-04-14 16:16:06 +0900

    Add the add card page

--- memory-cards/scripts.js
+++ memory-cards/scripts.js
@@ -1,1 +1,9 @@
-/* empty */
+let addPage = document.querySelector("#add-container");
+
+document.querySelector("#add").onclick = function() {
+  addPage.classList.add("show");
+}
+
+document.querySelector("#close").onclick = function () {
+  addPage.classList.remove("show");
+}
