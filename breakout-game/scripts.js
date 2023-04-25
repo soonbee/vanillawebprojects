f8da27ec5d2ba0fa4c2c3b6f91f74c4f4a0478b5
soonbee <maru204@gmail.com>
2023-04-25 10:41:38 +0900

    Add paddle

--- breakout-game/scripts.js
+++ breakout-game/scripts.js
@@ -8,6 +8,12 @@ let xVector = 1;
 let yVector = 1;
 let radius = 10;
 
+// paddle spec
+let paddleWidth = 80;
+let paddleHeight = 10;
+let paddleSpeed = 0;
+let paddleMovement = 0;
+
 // 5 row 9 col
 let bricks = Array.from({length: 45}, (_, idx) => idx).map(el => ({
     x: 45 + 80 * (el % 9), y: 60 + 30 * Math.floor(el / 9), w: 70, h: 20
@@ -21,6 +27,22 @@ function animate(){
     bricks.forEach(brick => {
         ctx.fillRect(brick.x, brick.y, brick.w, brick.h);
     })
+
+    // draw paddle
+    ctx.fillStyle = "#0095dd";
+    // move paddle position
+    paddleMovement += paddleSpeed;
+    paddleMovement = Math.max(paddleMovement, paddleWidth / 2 - canvas.width / 2);
+    paddleMovement = Math.min(paddleMovement, canvas.width / 2 - paddleWidth / 2);
+
+    // draw paddle
+    ctx.fillStyle = "#0095dd";
+    ctx.fillRect(
+        canvas.width / 2 - paddleWidth / 2 + paddleMovement,
+        canvas.height - 20,
+        paddleWidth,
+        paddleHeight,
+    )
 
     // detect collision
     if (xPosition > canvas.width - radius || xPosition < radius) {
@@ -43,5 +65,18 @@ function animate(){
     // loop animate
     requestAnimationFrame(animate);
 }
+
+window.addEventListener("keydown", function(e) {
+    let key = e.key || e.keyCode;
+    if (key === "ArrowLeft" || key === 37) {
+        paddleSpeed = -10;
+    } else if (key === "ArrowRight" || key === 39) {
+        paddleSpeed = 10;
+    }
+});
+
+window.addEventListener("keyup", function(e) {
+    paddleSpeed = 0;
+});
 
 animate(); 
