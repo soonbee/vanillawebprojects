5f81dd8b3219a9d0a47ded013cb3167ec73e6c9b
soonbee <maru204@gmail.com>
2023-04-25 10:03:13 +0900

    Add bricks

--- breakout-game/scripts.js
+++ breakout-game/scripts.js
@@ -8,10 +8,19 @@ let xVector = 1;
 let yVector = 1;
 let radius = 10;
 
+// 5 row 9 col
+let bricks = Array.from({length: 45}, (_, idx) => idx).map(el => ({
+    x: 45 + 80 * (el % 9), y: 60 + 30 * Math.floor(el / 9), w: 70, h: 20
+}));
 
 function animate(){
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#0095dd";
+    bricks.forEach(brick => {
+        ctx.fillRect(brick.x, brick.y, brick.w, brick.h);
+    })
 
     // detect collision
     if (xPosition > canvas.width - radius || xPosition < radius) {
