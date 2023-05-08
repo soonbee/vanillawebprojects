041b06c8fd2c637b20b8f686930e1bfe6ed393cb
soonbee <maru204@gmail.com>
2023-05-08 20:37:48 +0900

    Add ui

--- sortable-list/scripts.js
+++ sortable-list/scripts.js
@@ -1,1 +1,26 @@
-/* empty */
+const cardList = document.querySelector(".card-list");
+
+const names = [
+    "Larry Ellison",
+    "Amancio Ortega",
+    "Carlos Slim Helu",
+    "Larry Page",
+    "Warren Buffett",
+    "Michael Bloomberg",
+    "Bernard Arnault",
+    "Bill Gates",
+    "Mark Zuckerberg",
+    "Jeff Bezos",
+];
+let currentSequence = arrayShuffle(names);
+
+cardList.innerHTML = currentSequence.map((name, idx) => {
+    return `\
+<div class="card">
+  <div class="number">${idx + 1}</div>
+  <div class="draggable-content" draggable="true">
+    <div class="name">${name}</div>
+    <div class="draggable-indicator">=</div>
+  </div>
+</div>`
+}).join("");
